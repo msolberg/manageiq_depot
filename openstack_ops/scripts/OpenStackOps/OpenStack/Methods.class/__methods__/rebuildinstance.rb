2cfe31efd33c1498c3e3b03867f0af3ab3ca2be5
Michael Solberg <msolberg@redhat.com>
2015-01-20 14:24:35 -0500

    Scope fog connections to the virtual machine's tenant. Fixes msolberg/manageiq_depot#1.

--- openstack_ops/scripts/OpenStackOps/OpenStack/Methods.class/__methods__/rebuildinstance.rb
+++ openstack_ops/scripts/OpenStackOps/OpenStack/Methods.class/__methods__/rebuildinstance.rb
@@ -6,17 +6,28 @@ require 'fog'
 
 vm = $evm.root['vm']
 openstack = vm.ext_management_system
+
+tenant_name = nil
+openstack.cloud_tenants.each do |tenant|
+  if tenant.id == vm.cloud_tenant_id
+    tenant_name = tenant.name
+  end
+end
+
+if tenant_name == nil
+  $evm.log("error", "Couldn't find tenant #{vm.cloud_tenant_id} associated with vm #{vm.name}")
+  exit MIQ_ABORT
+end
+
 auth_url = "http://#{openstack[:hostname]}:#{openstack[:port]}/v2.0/tokens"
 
-# TODO: This should be scoped to the tenant of the virtual machine
-# instance and not "admin"
 begin
   conn = Fog::Compute.new({
     :provider => 'OpenStack',
     :openstack_api_key  => openstack.authentication_password,
     :openstack_username => openstack.authentication_userid,
     :openstack_auth_url => auth_url,
-    :openstack_tenant   => "admin"
+    :openstack_tenant   => tenant_name
   })
 rescue => connerr
   $evm.log("error", "Couldn't connect to Openstack with provider credentials")
